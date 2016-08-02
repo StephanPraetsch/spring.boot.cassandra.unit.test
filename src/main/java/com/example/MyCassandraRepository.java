517838ab823924a812e107eced5ffb7438f8747a
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2016-08-02 14:03:12 +0200

    MyCassandraRepository

--- src/main/java/com/example/MyCassandraRepository.java
+++ src/main/java/com/example/MyCassandraRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskConfigurationRepository extends
-        CrudRepository<MyCassandraEntry, MyCassandraKey> {
+public interface MyCassandraRepository extends CrudRepository<MyCassandraEntry, MyCassandraKey> {
     // nothing
 }
