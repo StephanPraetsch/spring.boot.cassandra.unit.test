aa5e3e310666912cefd41f2de004bbccfa9cf56f
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2016-08-02 15:19:22 +0200

    DependencyInjectionTestExecutionListener, no default-context

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -2,7 +2,7 @@ package com.example;
 
 import org.cassandraunit.spring.CassandraDataSet;
 import org.cassandraunit.spring.CassandraUnit;
-import org.cassandraunit.spring.CassandraUnitTestExecutionListener;
+import org.cassandraunit.spring.CassandraUnitDependencyInjectionTestExecutionListener;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,14 +10,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 /*
  * https://github.com/jsevellec/cassandra-unit/wiki/Spring-for-Cassandra-unit
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ComponentScan
-@ContextConfiguration(value = { "classpath:/default-context.xml" })
-@TestExecutionListeners({ CassandraUnitTestExecutionListener.class })
+@ContextConfiguration
+@TestExecutionListeners({ CassandraUnitDependencyInjectionTestExecutionListener.class,
+        DependencyInjectionTestExecutionListener.class })
 @CassandraDataSet(value = { "setupTables.cql" }, keyspace = "keyspaceToCreate")
 @CassandraUnit
 public class ApplicationTests {
