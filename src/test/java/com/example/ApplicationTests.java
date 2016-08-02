16604de56d4f180a04ab2107915311b7ffaa1bd2
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2016-08-02 12:43:41 +0200

    with cassandra-unit-spring

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,12 +1,24 @@
 package com.example;
 
+import org.cassandraunit.spring.CassandraDataSet;
+import org.cassandraunit.spring.CassandraUnit;
+import org.cassandraunit.spring.CassandraUnitTestExecutionListener;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+/*
+ * https://github.com/jsevellec/cassandra-unit/wiki/Spring-for-Cassandra-unit
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ComponentScan
+@ContextConfiguration(value = { "classpath:/default-context.xml" })
+@TestExecutionListeners({ CassandraUnitTestExecutionListener.class })
+@CassandraDataSet(value = { "setupTables.cql" }, keyspace = "keyspaceToCreate")
+@CassandraUnit
 public class ApplicationTests {
 
     @Test
