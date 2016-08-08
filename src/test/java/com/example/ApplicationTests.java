586a06ff5a31b5f867acdd6f66da27b4e552bbbb
Stephan Praetsch <Stephan.Praetsch@mercateo.com>
2016-08-08 11:11:47 +0200

    working example with cassandra-unit 2.1.9.2

--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -17,6 +18,8 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
  * https://github.com/jsevellec/cassandra-unit/wiki/Spring-for-Cassandra-unit
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest({ "spring.data.cassandra.port=9142",
+        "spring.data.cassandra.keyspace-name=keyspaceToCreate" })
 @EnableAutoConfiguration
 @ComponentScan
 @ContextConfiguration
